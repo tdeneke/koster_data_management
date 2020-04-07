60dcfea928afd49fd87e3e21d6d16d3034776f54
Jannes <jurie.germishuys@combine.se>
2020-04-07 15:25:32 +0200

    Added fixes to db_static to work with current file structure

--- zooniverse_scripts/db_static.py
+++ zooniverse_scripts/db_static.py
@@ -8,57 +8,67 @@ from db_setup import *
 from zooniverse_setup import *
 
 
-def add_movies(file)
-	
-	movies_df = pd.read_csv(file)
+def get_site_id(row):
 
-	# Set up sites information
-	sites_df = movies_df[['SiteDecription', 'CentroidLat', 'CentroidLong']].groupby(['CentroidLat', 'CentroidLong']).min().reset_index()
-	sites_db = sites_df[['SiteDecription', 'CentroidLat', 'CentroidLong']]
+	# Currently we discard sites that have no lat or lon coordinates, since site descriptions are not unique
+	# it becomes difficult to match this information otherwise
 
-	# Update sites table
-	conn = create_connection(args.db_path)
+	try:
+		site_id = retrieve_query(conn, f"SELECT id FROM sites WHERE coord_lat=={row['CentroidLat']} AND coord_lon=={row['CentroidLong']}")[0][0]
+	except:
+		site_id = 0
+	return site_id
+
+
+def add_movies(file, db_path):
+
+    movies_df = pd.read_csv(file)
+
+    # Set up sites information
+    sites_df = movies_df[['SiteDecription', 'CentroidLat', 'CentroidLong']].groupby(['CentroidLat', 'CentroidLong']).min().reset_index()
+    sites_db = sites_df[['SiteDecription', 'CentroidLat', 'CentroidLong']]
+    
+    # Update sites table
+    conn = create_connection(db_path)
 
     try:
-        insert_many(conn, [tuple(i) + (None, ) for i in site_db.values], "sites", 5)
+    	# An additional None is added at the front to account for the autoincrement id column
+        insert_many(conn, [(None, ) + tuple(i) + (None, ) for i in sites_db.values], "sites", 5)
     except sqlite3.Error as e:
         print(e)
-
+    
     conn.commit()
-
+    
     # Reference with sites table
-
-	movies_df['Site_id'] = movies_df.apply(lambda x: execute_query(f"SELECT id FROM sites WHERE coord_lat=={x['CentroidLat']} AND coord_lon=={x['CentroidLong']} "))
-
-	movies_db = movies_df[['FilenameCurrent', 'DateFull', 'Total_time', 'Author', 'Site_id']]
-
-	# Update movies table
-	conn = create_connection(args.db_path)
-
+    
+    movies_df['Site_id'] = movies_df.apply(lambda x: get_site_id(x), 1)
+    movies_db = movies_df[['FilenameCurrent', 'DateFull', 'Total_time', 'Author', 'Site_id']]
+    
+    # Update movies table
+    conn = create_connection(db_path)
+    
     try:
-        insert_many(conn, [tuple(i) + (None, ) for i in movies_db.values], "movies", 6)
+    	insert_many(conn, [(None, ) + tuple(i) + (None, ) for i in movies_db.values], "movies", 7)
     except sqlite3.Error as e:
         print(e)
-
+    
     conn.commit()
-
+    
     print('Updated sites and movies')
+    
+def add_species(file, db_path):
 
+    species_df = pd.read_csv(file)
 
-
-def add_species(file):
-
-	species_df = pd.read_csv(file)
-
-	# Update movies table
-	conn = create_connection(args.db_path)
-
+    # Update movies table
+    conn = create_connection(db_path)
+    
     try:
-        insert_many(conn, [tuple(i) for i in species_df['Name'].values], "species", 1)
+    	# An additional None is added at the front to account for the autoincrement id column
+    	insert_many(conn, [(None, ) + tuple([i]) for i in species_df['Name'].values], "species", 2)
     except sqlite3.Error as e:
         print(e)
-
+    
     conn.commit()
-
+    
     print('Updated species')
-
