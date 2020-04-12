7d628395bcd0e268dd435e307ee7036e3e582869
victor-wildlife <victor@wildlife.ai>
2020-04-13 10:17:30 +1200

    Added section to download csv files

--- zooniverse_scripts/db_static.py
+++ zooniverse_scripts/db_static.py
@@ -8,6 +8,31 @@ from db_setup import *
 from zooniverse_setup import *
 
 
+def download_csv_from_google_drive(id):
+    
+    # Download the csv files stored in Google Drive with initial information about
+    # the movies and the species
+    
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    return response
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
 def get_site_id(row):
 
     # Currently we discard sites that have no lat or lon coordinates, since site descriptions are not unique
@@ -16,16 +41,18 @@ def get_site_id(row):
     try:
         site_id = retrieve_query(
             conn,
-            f"SELECT id FROM sites WHERE coord_lat=={row['CentroidLat']} AND coord_lon=={row['CentroidLong']}",
+            f"SELECT id FROM sites WHERE coord_lat=={row['CentroidLat']} AND coord_lon=={row['CentroidLong']}"
         )[0][0]
     except:
         site_id = 0
     return site_id
 
 
-def add_movies(file, db_path):
+def add_movies(gdrive_file_id, db_path):
 
-    movies_df = pd.read_csv(file)
+    # Download the csv with movies information from the google drive
+    movies_csv_resp = download_csv_from_google_drive(gdrive_file_id)
+    movies_df = pd.read_csv(io.StringIO(movies_csv_resp.content.decode('utf-8')))
 
     # Set up sites information
     sites_df = (
@@ -71,9 +98,11 @@ def add_movies(file, db_path):
     print("Updated sites and movies")
 
 
-def add_species(file, db_path):
+def add_species(gdrive_file_id, db_path):
 
-    species_df = pd.read_csv(file)
+    # Download the csv with movies information from the google drive
+    species_csv_resp = download_csv_from_google_drive(gdrive_file_id)
+    species_df = pd.read_csv(io.StringIO(species_csv_resp.content.decode('utf-8')))
 
     # Update movies table
     conn = create_connection(db_path)
