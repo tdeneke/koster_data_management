0e7c75276014c5f84304e47589eba4d88b7bd7a3
victor-wildlife <victor@wildlife.ai>
2020-04-18 08:41:48 +1200

    updated to match fields used in db_static

--- zooniverse_scripts/schema.py
+++ zooniverse_scripts/schema.py
@@ -14,8 +14,6 @@ filename text NULL,
 created_on datetime NULL,
 duration datetime NULL,
 author text NULL,
-location text NULL,
-organisation text NULL,
 path text NULL,
 site_id integer,
 FOREIGN KEY (site_id) REFERENCES sites (id)
