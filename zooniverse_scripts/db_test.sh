ab3eeba60413aafa587fa15086e8b15059162142
Jannes <jurie.germishuys@combine.se>
2020-04-21 16:17:03 +0200

    Small fix to bash script

--- zooniverse_scripts/db_test.sh
+++ zooniverse_scripts/db_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm $3 &&
-python db_setup.py --db_path $3
+python db_setup.py --db_path $3 &&
 python db_static.py --species_file_id '1dnueH3BjJrMK8buVjfyFbxfu0E-5dX7Z' --movies_file_id '1LL-Ah_FIkBiGKEldYvuhNeL2NyOvKBip' --db_path $3 &&
 python manual_upload.py -u $1 -p $2 -db $3 &&
 python process_clips.py -u $1 -p $2 -db $3
