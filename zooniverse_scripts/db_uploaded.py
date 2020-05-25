46622f827957401ff36d3cca4e5b83c7e000829c
victor-wildlife <victor@wildlife.ai>
2020-05-25 15:58:47 +1200

    updates clips and frames manually and automatically uploaded to Zoo

--- zooniverse_scripts/db_uploaded.py
+++ zooniverse_scripts/db_uploaded.py
@@ -7,6 +7,83 @@ from datetime import datetime
 from panoptes_client import Project, Panoptes
 from zooniverse_setup import auth_session
 
+# Function to extract the metadata from subjects df
+def extract_metadata(subj_df):
+    
+    # Reset index of df
+    subj_df = subj_df.reset_index(drop=True).reset_index()
+    
+    # Flatten the metadata information
+    meta_df = pd.json_normalize(subj_df.metadata.apply(json.loads))
+
+    # Drop metadata and index columns from original df
+    subj_df = subj_df.drop(columns=["metadata", "index",])
+    
+    return subj_df, meta_df
+
+# Function to get the movie_ids based on the movie filenames
+def get_movies_id(df, db_path):
+    
+    # Create connection to db
+    conn = db_utils.create_connection(db_path)
+
+    # Query id and filenames from the movies table
+    movies_df = pd.read_sql_query("SELECT id, filename FROM movies", conn)
+    movies_df = movies_df.rename(
+        columns={"id": "movie_id", "filename": "movie_filename"}
+    )
+
+    # Unswedify movie filenames
+    movies_df["movie_filename"] = movies_df["movie_filename"].str.normalize("NFD")
+    df["movie_filename"] = df["movie_filename"].str.normalize("NFD")
+
+    # Reference the manually uploaded subjects with the movies table
+    df = pd.merge(
+        df, movies_df, how="left", on="movie_filename"
+    )
+    
+    # Drop the movie_filename column
+    df = df.drop(columns=["movie_filename",])
+    
+    return df
+
+# Function to process the metadata of manually uploaded clips
+def process_manual_clips(meta_df, db_path):
+    
+    # Select the filename of the clips
+    clip_filenames = meta_df["filename"]
+
+    # Get the starting time of clips in relation to the original movie
+    # split the filename, select the last section, and remove the extension type
+    meta_df["clip_start_time"] = (
+        clip_filenames.str.rsplit("_", 1).str[-1].str.replace(".mp4", "")
+    )
+
+    # Extract the filename of the original movie
+    meta_df["movie_filename_ext"] = meta_df.apply(
+        lambda x: x["filename"].replace("_" + x["clip_start_time"], ""), axis=1
+    )
+
+    # Remove the extension of the filename of the original movie
+    meta_df["movie_filename"] = meta_df["movie_filename_ext"].str.replace(
+        ".mp4", ""
+    )
+    
+    # Get the end time of clips in relation to the original movie
+    meta_df["clip_start_time"] = pd.to_numeric(
+        meta_df["clip_start_time"], downcast="signed"
+    )
+    meta_df["clip_end_time"] = meta_df["clip_start_time"] + 10
+
+    # Select only relevant columns
+    meta_df = meta_df[
+        ["filename", "movie_filename", "clip_start_time", "clip_end_time"]
+    ]
+    
+    # Include movie_ids to the metadata
+    meta_df = get_movies_id(meta_df, db_path)
+    
+    return meta_df
 
 def main():
 
@@ -53,108 +130,71 @@ def main():
     # Add a column that diferentiates clips from frames
     conditions = [
         (subjects_df["metadata"].str.contains(".mp4")),
-        (subjects_df["metadata"].str.contains(".jpg")),
+        (subjects_df["metadata"].str.contains("frame_")),
     ]
     choices = ["clip", "frame"]
     subjects_df["subject_type"] = np.select(conditions, choices, default="unknown")
 
-    # Specify dates when finalised clips and frames started to get uploaded
-    first_clips_date = "2019-11-17 00:00:00 UTC"
-    first_frames_date = "2020-06-12 00:00:00 UTC"
-
+    ### Clip subjects ###
+    
     # Select clip subjects
-    clips_df = (
-        subjects_df[
-            (subjects_df.subject_type == "clip")
-            & (first_clips_date <= subjects_df.created_at)
-        ]
-        .reset_index(drop=True)
-        .reset_index()
+    clips_df = subjects_df[subjects_df["subject_type"].str.contains("clip")]
+        
+    # Specify the dates when clips were manually uploaded
+    first_manual_clips = "2019-11-17 00:00:00 UTC"
+    last_manual_clips = "2020-05-01 00:00:00 UTC"
+    
+    # Select manually uploaded clips
+    man_clips_df = clips_df[clips_df["created_at"].between(first_manual_clips,last_manual_clips)]
+    
+    # Extract metadata from manually uploaded clips
+    man_clips_df, man_clips_meta = extract_metadata(man_clips_df)
+        
+    # Process the metadata of manually uploaded clips    
+    man_clips_meta = process_manual_clips(man_clips_meta, args.db_path)
+    
+    # Combine metadata info with the subjects df
+    man_clips_df = pd.concat([man_clips_df, man_clips_meta], axis=1)
+    
+    ### Frame subjects ###
+        
+    # Select frame subjects
+    frames_df = subjects_df[subjects_df["subject_type"].str.contains("frame")]
+        
+    # Specify the date range when frames were manually uploaded
+    first_manual_frames = "2019-11-17 00:00:00 UTC"
+    last_manual_frames = "2020-05-25 00:00:00 UTC"    
+    
+    # Select manually uploaded frames
+    man_frames_df = frames_df[frames_df["created_at"].between(first_manual_frames,last_manual_frames)]
+        
+    # Extract metadata from manually uploaded frames
+    man_frames_df, man_frames_meta = extract_metadata(man_frames_df)
+        
+    # Rename columns to match the subjects table    
+    man_frames_meta = man_frames_meta.rename(
+        columns={"movie_frame": "frame_number"}
     )
-
-    # Flatten the metadata information
-    clips_metadata = pd.json_normalize(clips_df.metadata.apply(json.loads))
-
-    # Select the filename of the clips
-    clip_filenames = clips_metadata["filename"]
-
-    # Get the starting time of clips in relation to the original movie
-    # split the filename, select the last section, and remove the extension type
-    clips_metadata["clip_start_time"] = (
-        clip_filenames.str.rsplit("_", 1).str[-1].str.replace(".mp4", "")
-    )
-
-    # Extract the filename of the original movie
-    clips_metadata["movie_filename_ext"] = clips_metadata.apply(
-        lambda x: x["filename"].replace("_" + x["clip_start_time"], ""), axis=1
-    )
-
-    # Remove the extension of the filename of the original movie
-    clips_metadata["movie_filename"] = clips_metadata["movie_filename_ext"].str.replace(
-        ".mp4", ""
-    )
-
-    # Get the end time of clips in relation to the original movie
-    clips_metadata["clip_start_time"] = pd.to_numeric(
-        clips_metadata["clip_start_time"], downcast="signed"
-    )
-    clips_metadata["clip_end_time"] = clips_metadata["clip_start_time"] + 10
-
-    # Select only relevant columns
-    clips_metadata = clips_metadata[
-        ["filename", "movie_filename", "clip_start_time", "clip_end_time"]
-    ]
-
-    # Create connection to db
-    conn = db_utils.create_connection(args.db_path)
-
-    # Query id and filenames from the movies table
-    movies_df = pd.read_sql_query("SELECT id, filename FROM movies", conn)
-    movies_df = movies_df.rename(
-        columns={"id": "movie_id", "filename": "movie_filename"}
-    )
-
-    # Deal with special characters
-    movies_df["movie_filename"] = movies_df["movie_filename"].str.normalize("NFD")
-    clips_metadata["movie_filename"] = clips_metadata["movie_filename"].str.normalize(
-        "NFD"
-    )
-
-    # Reference the clips with the movies table
-    clips_metadata = pd.merge(
-        clips_metadata, movies_df, how="left", on="movie_filename"
-    )
-
-    # Combine the metadata information
-    clips_df = pd.concat([clips_df, clips_metadata], axis=1)
-
-    # Drop unnecessary columns
-    clips_df = clips_df.drop(columns=["index", "metadata", "movie_filename",])
-
-    # Select frames subjects
-    frames_df = subjects_df[
-        (subjects_df.subject_type == "frame")
-        & (first_frames_date <= subjects_df.created_at)
-    ]
-
-    if len(frames_df) > 0:
-        # Flatten the metadata information
-        frames_metadata = pd.json_normalize(frames_df.metadata.apply(json.loads))
-
-        # Combine the metadata information
-        frames_df = pd.concat([frames_df, frames_metadata], axis=1)
-
-        # Combine the frame and clip subjects
-        subjects = pd.merge(clips_df, frames_df, how="outer")
-
-    else:
-        # Include frame-specific columns
-        subject_columns = pd.read_sql_query("SELECT * FROM subjects", conn)
-        frames_df = frames_df.append(subject_columns.filter(regex="frame"))
-
-        # Combine the frame and clip subjects
-        subjects = pd.merge(clips_df, frames_df, how="outer")
-
+    
+    # Combine metadata info with the subjects df
+    man_frames_df = pd.concat([man_frames_df, man_frames_meta], axis=1)
+        
+    # Select automatically uploaded frames
+    #auto_frames_df = frames_df[frames_df["created_at"]>last_manual_frames]
+    
+    # Extract metadata from automatically uploaded frames
+    #auto_frames_df, auto_frames_meta = extract_metadata(auto_frames_df)
+    
+    # Combine metadata info with the subjects df
+    #auto_frames_df = pd.concat([auto_frames_df, auto_frames_meta], axis=1)
+        
+    # TODO Check movie_ids of automatically uploaded subjects are correct
+    
+    ### Update subjects table ###
+    
+    # Combine all uploaded subjects
+    subjects = pd.merge(man_clips_df, man_frames_df, how="outer")
+    
     # Set subject_id information as id
     subjects = subjects.rename(columns={"subject_id": "id",})
 
