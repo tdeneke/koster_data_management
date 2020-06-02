455c131feef0136d463b53e5b4511b0239113b2d
Jannes <jurie.germishuys@combine.se>
2020-06-02 13:38:50 +0200

    Added function to draw multiple bounding boxes

--- zooniverse_scripts/frame_utils.py
+++ zooniverse_scripts/frame_utils.py
@@ -6,17 +6,21 @@ import numpy as np
 import cv2 as cv
 import pandas as pd
 
-# Draw the predicted bounding box
-def drawBox(movie_dir, boxes, img_path, out_path):
-	movie_file = movie_dir + os.path.basename(img_path.rsplit("_frame_")[0]) + ".mov"
-	frame_number = int(re.findall(r"(?<=[_frame_])\d+(?=_)", img_path)[0])
-	frame = pims.Video(movie_file)[frame_number]
-    for i in range(0, len(boxes)):
-    	# Calculating end-point of bounding box based on starting point and w, h
-    	end_box = tuple(boxes[i][0] + boxes[i][2], boxes[i][1] + boxes[i][3]) 
-        # changed color and width to make it visible
-        cv.rectangle(frame, tuple(boxes[i][:2]), end_box, (255, 0, 0), 1)
-    cv.imwrite(out_path+os.path.basename(img_path), frame)
+
+def drawBoxes(df, movie_dir, out_path):
+	df["movie_path"] = movie_dir + df["filename"].apply(lambda x: os.path.basename(x.rsplit("_frame_")[0]) + ".mov")
+	df["annotation"] = df[["x_position", "y_position", "width", "height"]].apply(lambda x: tuple(x[0], x[1], x[2], x[3]))
+	df = df.drop(columns=["x_position", "y_position", "width", "height"])
+	for name, group in df.groupby(["movie_path", "frame_number", "species_id", "filename"]):
+		frame = pims.Video(name[0])[name[1]]
+		for box in group.values:
+	    	# Calculating end-point of bounding box based on starting point and w, h
+	    	end_box = tuple([box[0] + box[2], box[1] + box[3]]) 
+	        # changed color and width to make it visible
+		    cv.rectangle(frame, tuple(box[:2]), end_box, (255, 0, 0), 1)
+		if not os.path.exists(out_path): os.mkdir(out_path)
+    	cv.imwrite(out_path+os.path.basename(name[3]), frame)
+
 
 
 
