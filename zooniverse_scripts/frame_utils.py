ff5587d6d3cd78a73a6a811691db0c3ca45c480b
Jannes <jurie.germishuys@combine.se>
2020-06-02 14:29:12 +0200

    Fixed drawBoxes utility function

--- zooniverse_scripts/frame_utils.py
+++ zooniverse_scripts/frame_utils.py
@@ -8,21 +8,23 @@ import pandas as pd
 
 
 def drawBoxes(df, movie_dir, out_path):
-	df["movie_path"] = movie_dir + df["filename"].apply(lambda x: os.path.basename(x.rsplit("_frame_")[0]) + ".mov")
-	df["annotation"] = df[["x_position", "y_position", "width", "height"]].apply(lambda x: tuple(x[0], x[1], x[2], x[3]))
-	df = df.drop(columns=["x_position", "y_position", "width", "height"])
-	for name, group in df.groupby(["movie_path", "frame_number", "species_id", "filename"]):
-		frame = pims.Video(name[0])[name[1]]
-		for box in group.values:
-	    	# Calculating end-point of bounding box based on starting point and w, h
-	    	end_box = tuple([box[0] + box[2], box[1] + box[3]]) 
-	        # changed color and width to make it visible
-		    cv.rectangle(frame, tuple(box[:2]), end_box, (255, 0, 0), 1)
-		if not os.path.exists(out_path): os.mkdir(out_path)
-    	cv.imwrite(out_path+os.path.basename(name[3]), frame)
-
-
-
-
-
-
+    df["movie_path"] = movie_dir + df["filename"].apply(
+        lambda x: os.path.basename(x.rsplit("_frame_")[0]) + ".mov"
+    )
+    df["annotation"] = df[["x_position", "y_position", "width", "height"]].apply(
+        lambda x: tuple([x[0], x[1], x[2], x[3]]), 1
+    )
+    df = df.drop(columns=["x_position", "y_position", "width", "height"])
+    for name, group in df.groupby(
+        ["movie_path", "frame_number", "species_id", "filename"]
+    ):
+        frame = pims.Video(name[0])[name[1]]
+        boxes = [tuple(i[4:])[0] for i in group.values]
+        for box in boxes:
+            # Calculating end-point of bounding box based on starting point and w, h
+            end_box = tuple([int(box[0] + box[1]), int(box[2] + box[3])])
+            # changed color and width to make it visible
+            cv.rectangle(frame, (int(box[0]), int(box[1])), end_box, (255, 0, 0), 1)
+        if not os.path.exists(out_path):
+            os.mkdir(out_path)
+        cv.imwrite(out_path + os.path.basename(name[3]), frame)
