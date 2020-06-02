ded4093dce01054eb7da9673e9df81d3e4a27d97
Jannes <jurie.germishuys@combine.se>
2020-06-02 18:45:18 +0200

    Added main function

--- zooniverse_scripts/frame_utils.py
+++ zooniverse_scripts/frame_utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 
 def drawBoxes(df, movie_dir, out_path):
-    df["movie_path"] = movie_dir + df["filename"].apply(
+    df["movie_path"] = movie_dir + "/" df["filename"].apply(
         lambda x: os.path.basename(x.rsplit("_frame_")[0]) + ".mov"
     )
     df["annotation"] = df[["x_position", "y_position", "width", "height"]].apply(
@@ -28,3 +28,41 @@ def drawBoxes(df, movie_dir, out_path):
         if not os.path.exists(out_path):
             os.mkdir(out_path)
         cv.imwrite(out_path + os.path.basename(name[3]), frame)
+
+
+
+def main():
+    "Handles argument parsing and launches the correct function."
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+    "-db",
+    "--db_path",
+    type=str,
+    help="the absolute path to the database file",
+    default=r"koster_lab.db",
+    required=True,
+    )
+    parser.add_argument(
+    "-m",
+    "--movie_dir",
+    type=str,
+    help="the directory of movie files",
+    default=r"/uploads/",
+    required=True,
+    )
+    parser.add_argument(
+    "-o",
+    "--output_dir",
+    type=str,
+    help="the directory to save the frames",
+    default=r"/database/frames/",
+    required=True,
+    )
+    args = parser.parse_args()
+    conn = db_utils.create_connection(args.db_path)
+    df = pd.read_sql_query("SELECT b.filename, b.frame_number, a.species_id, a.x_position, a.y_position, a.width, a.height FROM agg_annotations_frame AS a LEFT JOIN subjects AS b ON a.subject_id=b.id", conn);
+    drawBoxes(df, args.movie_dir, args.output_dir)
+
+
+if __name__ == '__main__':
+    main()
