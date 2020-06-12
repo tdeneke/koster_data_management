20185577d44b2c524f0fbb7bfb889a5d715c3e5c
victor-wildlife <victor@wildlife.ai>
2020-06-12 15:50:42 +1200

    updated and cleaned the extraction of frames

--- upload_subjects/upload_frames.py
+++ upload_subjects/upload_frames.py
@@ -1,5 +1,6 @@
 import argparse, os, cv2, re
-import db_utils, clip_utils
+import utils.db_utils as db_utils
+import utils.clip_utils as clip_utils
 import pandas as pd
 import numpy as np
 import pims
@@ -13,19 +14,11 @@ from panoptes_client import (
     Subject,
     Project,
     Panoptes,
-)  # needed to upload clips to Zooniverse
+)
 
-
-def get_fps(video_file):
-    video_path = video_file if os.path.isfile(video_file) else unswedify(video_file)
-    if isinstance(video_file, str):
-        fps = int(cv2.VideoCapture(video_file).get(cv2.CAP_PROP_FPS))
-    else:
-        fps = None
-    return fps
-
-
-def get_species_frames(species_id, conn):
+# Function to identify up to n number of frames per classified clip
+# that contains species of interest after the first time seen
+def get_species_frames(species_id, conn, frames_path, n_frames):
 
     # Find classified clips that contain the species of interest
     frames_df = pd.read_sql_query(
@@ -33,8 +26,11 @@ def get_species_frames(species_id, conn):
         conn,
     )
 
-    # Get information related to the clips
-    (frames_df["start_time"], frames_df["movie_id"],) = list(
+    # Add species id to the df
+    frames_df["frame_exp_sp_id"] = species_id
+    
+    # Get start time of the clips and ids of the original movies
+    (frames_df["clip_start_time"], frames_df["movie_id"],) = list(
         zip(
             *pd.read_sql_query(
                 f"SELECT clip_start_time, movie_id FROM subjects WHERE id IN {tuple(frames_df['subject_id'].values)} AND subject_type='clip'",
@@ -43,46 +39,36 @@ def get_species_frames(species_id, conn):
         )
     )
 
-    # Identify the first second in the original movie when the species appears
-    frames_df["first_seen_movie"] = frames_df["start_time"] + frames_df["first_seen"]
+    # Identify the second of the original movie when the species first appears
+    frames_df["first_seen_movie"] = frames_df["clip_start_time"] + frames_df["first_seen"]
 
-    # Get the filepath of the original movie
-    f_paths = pd.read_sql_query(f"SELECT id, fpath FROM movies", conn)
+    # Get the filepath and fps of the original movies
+    f_paths = pd.read_sql_query(f"SELECT id, fpath, fps FROM movies", conn)
 
+    # Ensure swedish characters don't cause issues
+    frames_df["fpath"] = frames_df["fpath"].apply(
+        lambda x: str(x) if os.path.isfile(str(x)) else db_utils.unswedify(str(x))
+    )
+    
+    # Include movies' filepath and fps to the df
     frames_df = frames_df.merge(f_paths, left_on="movie_id", right_on="id")
+    
+    # Specify if original movies can be found
+    frames_df['exists'] = frames_df['fpath'].map(os.path.isfile)
+    
+    if frames_df[~frames_df.exists].any():
+        print(
+            f"There are {len(frames_df) - frames_df.exists.sum()} frames with a missing movie"
+        )
+        
+    # Select only frames from movies that can be found
+    frames_df = frames_df[frames_df.exists]
+    
+    # Read all original movies
+    video_dict = {k: pims.Video(k) for k in frames_df["fpath"].unique()}
 
-    # Calculate the fps of the original movie
-    frames_df["fps"] = frames_df["fpath"].apply(get_fps, 1)
-
-    # Set the filename of the frames to extract
-    frames_df["frame_number"] = (
-        frames_df["first_seen_movie"] * frames_df["fps"]
-    ).astype(int)
-
-    # Add species id to the df
-    frames_df["frame_exp_sp_id"] = species_id
-
-    # Drop unnecessary columns
-    frames_df.drop(["subject_id"], inplace=True, axis=1)
-
-    return frames_df
-
-
-# Function to extract up to n frames from movies after the first time seen
-def extract_frames(df, frames_path, n_frames):
-
-    # read all videos
-    df["fpath"] = df["fpath"].apply(
-        lambda x: str(x) if os.path.isfile(str(x)) else unswedify(str(x))
-    )
-
-    video_dict = {k: pims.Video(k) for k in df["fpath"].unique()}
-
-    df["movie_filename"] = df["fpath"].apply(
-        lambda x: os.path.splitext(x)[0] if isinstance(x, str) else x, 1
-    )
-
-    df["frames"] = df[["fpath", "first_seen_movie", "fps"]].apply(
+    # Identify up to n frames of the species per movie
+    frames_df["frames"] = frames_df[["fpath", "first_seen_movie", "fps"]].apply(
         lambda x: video_dict[x["fpath"]][
             np.arange(
                 int(x["first_seen_movie"]) * int(x["fps"]),
@@ -96,7 +82,9 @@ def extract_frames(df, frames_path, n_frames):
         ],
         1,
     )
-    df["frame_names"] = df[
+    
+    # Set the filename of the frames
+    frames_df["filename"] = frames_df[
         ["movie_base", "first_seen_movie", "fps", "frame_exp_sp_id"]
     ].apply(
         lambda x: [
@@ -113,12 +101,21 @@ def extract_frames(df, frames_path, n_frames):
         1,
     )
 
-    # save frames to frame_names
-    for frame, frame_name in zip(df["frames"].explode(), df["frame_names"].explode()):
-        Image.fromarray(frame).save(f"{frame_name}")
+    print(frames_df.head())
+    
+    # Drop unnecessary columns
+    frames_df.drop(["subject_id"], inplace=True, axis=1)
+
+    return frames_df
+    
+def extract_frames(df):    
+
+    # Extract and save frames
+    for frame, filename in zip(df["frames"].explode(), df["filename"].explode()):
+        Image.fromarray(frame).save(f"{filename}")
 
     print("Frames extracted successfully")
-    return df["frame_names"]
+    return df["filename"]
 
 
 def main():
@@ -174,113 +171,101 @@ def main():
     # Connect to Zooniverse
     koster_project = auth_session(args.user, args.password)
 
-    # Get species name
+    # Get id of species of interest
     species_id = pd.read_sql_query(
         f"SELECT id FROM species WHERE label='{args.species}'", conn
     ).values[0][0]
 
-    # Get all movie_files and frame_numbers for species
-    annotation_df = get_species_frames(species_id, conn)
+    # Identify n number of frames per classified clip that contains species of interest 
+    sp_frames_df = get_species_frames(species_id, conn, args.frames_path, args.n_frames)
 
-    # Get info of frames already classified
+    # Get info of frames already uploaded
     uploaded_frames_df = pd.read_sql_query(
         f"SELECT movie_id, frame_number, frame_exp_sp_id FROM subjects WHERE frame_exp_sp_id='{species_id}' and subject_type='frame'",
         conn,
     )
 
+    # Filter out frames that have already been uploaded
     if len(uploaded_frames_df) > 0 and not args.testing:
 
         # Exclude frames that have already been uploaded
-        annotation_df = annotation_df[
-            ~(annotation_df["movie_id"].isin(uploaded_frames_df["movie_id"]))
-            & ~(annotation_df["frame_number"].isin(uploaded_frames_df["frame_number"]))
+        sp_frames_df = sp_frames_df[
+            ~(sp_frames_df["movie_id"].isin(uploaded_frames_df["movie_id"]))
+            & ~(sp_frames_df["frame_number"].isin(uploaded_frames_df["frame_number"]))
             & ~(
-                annotation_df["frame_exp_sp_id"].isin(
+                sp_frames_df["frame_exp_sp_id"].isin(
                     uploaded_frames_df["frame_exp_sp_id"]
                 )
             )
         ]
 
-    if len(annotation_df) == 0:
+    # Upload frames to Zooniverse that have not been uploaded
+    if len(sp_frames_df) == 0:
         print(
             "There are no subjects to upload, this may be because all of the subjects have already been uploaded"
         )
         raise
+     
+    else:
+        # Create the folder to store the frames if not exist
+        if not os.path.exists(args.frames_path):
+            os.mkdir(args.frames_path)
+        
+        # Extract the frames and save them
+        f_paths = extract_frames(sp_frames_df)
 
-    # Create the folder to store the frames if not exist
-    if not os.path.exists(args.frames_path):
-        os.mkdir(args.frames_path)
+        # Create a subjet set in Zooniverse to host the frames
+        subject_set = SubjectSet()
 
-    # Get valid movies base
-    annotation_df["movie_base"] = annotation_df["fpath"].apply(
-        lambda x: os.path.basename(str(x))
-        if os.path.isfile(x)
-        else unswedify(os.path.basename(str(x))),
-        1,
-    )
+        subject_set.links.project = koster_project
+        subject_set.display_name = args.species + date.today().strftime("_%d_%m_%Y")
 
-    annotation_df = annotation_df[
-        annotation_df["movie_base"].isin(os.listdir(args.movies_path))
-    ]
+        subject_set.save()
 
-    # Extract the frames and save them
-    f_paths = extract_frames(annotation_df, args.frames_path, n_frames)
+        # Save information relevant to the subjects table of the koster db
+        sp_frames_df["label"] = args.species
+        sp_frames_df["subject_type"] = "frame"
 
-    # Rename the filename column
-    annotation_df = annotation_df.rename(columns={"frame_names": "filename"})
+        sp_frames_df["metadata"] = sp_frames_df[
+            [
+                "fpath",
+                "frame_number",
+                "fps",
+                "movie_id",
+                "label",
+                "frame_exp_sp_id",
+                "subject_type",
+            ]
+        ].to_dict("r")
 
-    # Create a subjest in Zooniverse where the frames will be uploaded
-    subject_set = SubjectSet()
+        sp_frames_df["frame_paths"] = f_paths
 
-    subject_set.links.project = koster_project
-    subject_set.display_name = args.species + date.today().strftime("_%d_%m_%Y")
+        sp_frames_df = sp_frames_df.drop(
+            [i for i in sp_frames_df.columns if i not in ["metadata", "frame_paths"]], 1
+        ).dropna()
 
-    subject_set.save()
+        # Upload frames to Zooniverse (with metadata)
+        new_subjects = []
 
-    # Save information relevant to the subjects table of the koster db
-    annotation_df["label"] = args.species
-    annotation_df["subject_type"] = "frame"
+        for filename, metadata in tqdm(sp_frames_df.values):
 
-    annotation_df["metadata"] = annotation_df[
-        [
-            "fpath",
-            "frame_number",
-            "fps",
-            "movie_id",
-            "label",
-            "frame_exp_sp_id",
-            "subject_type",
-        ]
-    ].to_dict("r")
+            for f in range(len(filename)):
 
-    annotation_df["frame_paths"] = f_paths
+                metadata["filename"] = filename[f]
+                metadata["frame_number"] = metadata["frame_number"] + f * metadata["fps"]
 
-    annotation_df = annotation_df.drop(
-        [i for i in annotation_df.columns if i not in ["metadata", "frame_paths"]], 1
-    ).dropna()
+                subject = Subject()
 
-    # Upload frames to Zooniverse (with metadata)
-    new_subjects = []
+                subject.links.project = koster_project  # tutorial_project
+                subject.add_location(filename[f])
 
-    for filename, metadata in tqdm(annotation_df.values):
+                subject.metadata.update(metadata)
 
-        for f in range(len(filename)):
+                subject.save()
+                new_subjects.append(subject)
 
-            metadata["filename"] = filename[f]
-            metadata["frame_number"] = metadata["frame_number"] + f * metadata["fps"]
-
-            subject = Subject()
-
-            subject.links.project = koster_project  # tutorial_project
-            subject.add_location(filename[f])
-
-            subject.metadata.update(metadata)
-
-            subject.save()
-            new_subjects.append(subject)
-
-    # Upload frames
-    subject_set.add(new_subjects)
+        # Upload frames
+        subject_set.add(new_subjects)
 
 
 if __name__ == "__main__":
