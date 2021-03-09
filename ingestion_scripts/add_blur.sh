3cf0e527d69153da39403075a016d468a80ed8e8
Jannes <jurie.germishuys@combine.se>
2021-03-09 17:20:22 +0100

    Change extension to mov

--- ingestion_scripts/add_blur.sh
+++ ingestion_scripts/add_blur.sh
@@ -10,6 +10,6 @@ for name in $1; do
 	 [0:v]crop=iw:75:0:ih*(80/100),boxblur=luma_radius=5:chroma_radius=7:luma_power=1[b1]; \
 	 [0:v][b0]overlay=0:H*(5/100)[ovr0]; \
 	 [ovr0][b1]overlay=0:H*(80/100)[ovr1]" \
-	-map "[ovr1]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart "${name%.*_blurred}.mp4" 
+	-map "[ovr1]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart "${name%.*_blurred}.mov" 
 done
 
