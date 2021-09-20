20cde524f652b32e3778655028b006b088169ce5
victor-wildlife <victor@wildlife.ai>
2021-09-20 13:36:41 +1200

    fixed typo

--- utils/zooniverse_utils.py
+++ utils/zooniverse_utils.py
@@ -137,11 +137,15 @@ def populate_subjects(subjects, project_n, db_path):
     # Add values to subjects
     db_utils.add_to_table(db_path, "subjects", [tuple(i) for i in subjects.values], 14)
     
-    # Print how many subjects are in the db
+    ##### Print how many subjects are in the db
+    # Create connection to db
+    conn = db_utils.create_connection(db_path)
+    
     # Query id and subject type from the subjects table
     subjects_df = pd.read_sql_query("SELECT id, subject_type FROM subjects", conn)
     frame_subjs = subjects_df[subjects_df["subject_type"]=="frame"].shape[0]
     clip_subjs = subjects_df[subjects_df["subject_type"]=="clip"].shape[0]
+    
     print("The database has a total of", frame_subjs, "frame subjects and", clip_subjs, "clip subjects have been updated")
 
     return subjects
